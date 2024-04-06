345aee958be70b422eb5993bf424d262b69e33ba
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-04-06 00:03:35 -0600

    Función de la llamada a los gifs

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=3WePHuCs9xEIQZL6Pc2TizrvlWK1tXO9&q=${ category }&limit=10`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    console.log(gifs);
+    return gifs;
+
+}
